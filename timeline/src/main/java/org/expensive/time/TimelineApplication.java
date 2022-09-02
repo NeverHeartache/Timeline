3823d523d6003a42177f3580d6dd91a77b64e5d4
helei <2215694729@qq.com>
2022-09-02 09:28:06 +0800

    进行代码重构，使代码更合理更简洁

--- timeline/src/main/java/org/expensive/time/TimelineApplication.java
+++ timeline/src/main/java/org/expensive/time/TimelineApplication.java
@@ -7,37 +7,9 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class TimelineApplication {
-    private final FrameProxy frameProxy;
-    private final Timer timer;
-
-    public TimelineApplication() throws IOException {
-        frameProxy = new FrameProxy();
-        timer = new Timer("Timeline");
-    }
 
     public static void main(String[] args) throws IOException {
-        TimelineApplication app = new TimelineApplication();
-        app.loopShow();
-    }
-
-    public void loopShow(){
-        TimerTask timerTask = new TimerTask() {
-            @Override
-            public void run() {
-                frameProxy.frameShow();
-                java.awt.Toolkit.getDefaultToolkit().beep();
-                try {
-                    Thread.sleep(1000L * 5L);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                frameProxy.frameHide();
-            }
-        };
-        //
-        Long delay = 1000L * 60 * 10L;
-        //x minutes 之后继续执行
-        Long period = 1000L * 60 * 50L;
-        timer.scheduleAtFixedRate(timerTask, delay, period);
+        FrameProxy frameProxy = new FrameProxy();
+        frameProxy.initLoopShow(frameProxy);
     }
 }
--- timeline/src/main/java/org/expensive/time/jframe/FrameProxy.java
+++ timeline/src/main/java/org/expensive/time/jframe/FrameProxy.java
@@ -9,9 +9,12 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class FrameProxy {
     private JFrame globalFrame = null;
+    private final Timer timer = new Timer("Timeline");
     public FrameProxy() throws IOException {
         if(globalFrame == null ){
             globalFrame = new GlobalFrame();
@@ -100,6 +103,7 @@ public class FrameProxy {
             System.out.println("当前系统不支持系统托盘");
         }
     }
+
     protected static Image createImage(String path, String description) {
         URL imageURL = FrameProxy.class.getResource(path);
 
@@ -109,5 +113,26 @@ public class FrameProxy {
         } else {
             return (new ImageIcon(imageURL, description)).getImage();
         }
+    }
+
+    public void initLoopShow(FrameProxy frameProxy){
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                frameProxy.frameShow();
+                java.awt.Toolkit.getDefaultToolkit().beep();
+                try {
+                    Thread.sleep(1000L * 5L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                frameProxy.frameHide();
+            }
+        };
+        //
+        Long delay = 1000L * 60 * 10L;
+        //x minutes 之后继续执行
+        Long period = 1000L * 60 * 50L;
+        timer.scheduleAtFixedRate(timerTask, delay, period);
     }
 }
