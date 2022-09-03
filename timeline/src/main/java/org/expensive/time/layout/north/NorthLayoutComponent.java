1eba8791ea5c9ec282c19ad294cc8a0f8f4e5733
helei <2215694729@qq.com>
2022-09-03 12:27:53 +0800

    设置字体，添加spring核心依赖

--- timeline/src/main/java/org/expensive/time/layout/north/NorthLayoutComponent.java
+++ timeline/src/main/java/org/expensive/time/layout/north/NorthLayoutComponent.java
@@ -3,6 +3,7 @@ package org.expensive.time.layout.north;
 import org.expensive.time.toolkit.DateUtils;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.util.*;
 import java.util.Timer;
@@ -14,21 +15,26 @@ public class NorthLayoutComponent extends JPanel {
     public NorthLayoutComponent() {
         //动态时间
         Calendar calendar = Calendar.getInstance();
-        String date = calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT_FORMAT, new Locale("ZH"));
+        String date = calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, new Locale("ZH"));
         timeLabel = new JLabel(date + ":");
-        timeLabel.setHorizontalAlignment(JLabel.LEFT);// 水平对其方式：居中
-        timeLabel.setVerticalAlignment(JLabel.TOP);//垂直对其方式：顶部
-        Font font1 = new Font("SansSerif", Font.CENTER_BASELINE, 60);
+        timeLabel.setHorizontalTextPosition(JLabel.LEFT);// 水平对其方式：居中
+        timeLabel.setVerticalTextPosition(JLabel.CENTER);//垂直对其方式：顶部
+        Font font1 = new Font("宋体", Font.PLAIN, 60);
+        timeLabel.setForeground(new Color(000000));
         timeLabel.setFont(font1);
         displayArea = new JLabel("" + System.lineSeparator());//trailing
-        displayArea.setHorizontalAlignment(JLabel.CENTER);// 水平对其方式：居中
-        displayArea.setVerticalAlignment(JLabel.TOP);//垂直对其方式：顶部
-        Font font2 = new Font("SansSerif", Font.CENTER_BASELINE, 60);
+        displayArea.setHorizontalTextPosition(JLabel.CENTER);// 水平对其方式：居中
+        displayArea.setVerticalTextPosition(JLabel.CENTER);//垂直对其方式：顶部
+        Font font2 = new Font("仿宋", Font.BOLD, 60);
         displayArea.setFont(font2);
+        displayArea.setForeground(new Color(0x000000));
         //新建对象的时候就开始跑线程
         runTimePanel(); //  添加一个动态的时间
         add(timeLabel);
         add(displayArea);
+
+        //设置
+        setBackground(new Color(0xE5E5E5));
     }
 
     private void runTimePanel() {
