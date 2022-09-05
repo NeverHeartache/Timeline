b217f1c0d07ce8466042fa4a2621f2d54bc08151
NeverHeartache <2215694729@qq.com>
2022-09-05 21:21:47 +0800

    添加查询properties的测试用例；

--- bootstrap/src/test/java/org/expensive/time/PropertiesTest.java
+++ bootstrap/src/test/java/org/expensive/time/PropertiesTest.java
@@ -8,19 +8,30 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.io.IOException;
+
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(value = {"applicationContext.xml"})
+@ContextConfiguration(value = {"/applicationContext.xml"})
 public class PropertiesTest {
 
     @Autowired
     private GlobalPropertiesUtil propertiesUtil;
 
     @Test
-    public void getProperties() {
-//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-//        GlobalPropertiesUtil propertiesUtil = context.getBean("globalProperties", GlobalPropertiesUtil.class);
-        String value = propertiesUtil.getProperty("application.name");
-        System.out.println(value);
+    public void getProperties() throws IOException {
+        propertiesUtil.loadResource("config/global.properties");
+        String appName = propertiesUtil.getProperty("application.name");
+        String appVersion = propertiesUtil.getProperty("application.version");
+        String appDesc = propertiesUtil.getProperty("application.description");
+        System.out.println(appName);
+        System.out.println(appVersion);
+        System.out.println(appDesc);
     }
 
 }
+/**
+ * 结果如下：
+ * 时间轴
+ * 1.0.1
+ * 工作时间轴，要工作也要身体伸展
+ */
