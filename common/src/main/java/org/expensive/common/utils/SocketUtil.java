4ae4d8bab3058c842aca5c2d5d9c96de7e0481bf
NeverHeartache <2215694729@qq.com>
2022-09-05 21:48:15 +0800

    添加socket 工具类

--- common/src/main/java/org/expensive/common/utils/SocketUtil.java
+++ common/src/main/java/org/expensive/common/utils/SocketUtil.java
@@ -1,9 +1,29 @@
 package org.expensive.common.utils;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 
 public class SocketUtil {
 
     private Socket socket;
+    private final int defaultTimeOut = 50000;
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public void setSocket(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void openSocket(String host, int port) throws IOException {
+        socket.connect(new InetSocketAddress(host, port), defaultTimeOut);
+    }
+
+    public void openSocketWithTime(String host, int port, int timeout) throws IOException {
+        if (timeout <= 0) timeout = defaultTimeOut;
+        socket.connect(new InetSocketAddress(host, port), timeout);
+    }
 
 }
