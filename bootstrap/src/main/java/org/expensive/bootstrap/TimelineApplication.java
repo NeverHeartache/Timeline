955a4e0d6e05926b1cfde17a1c9b7d718a6e85dd
helei <2215694729@qq.com>
2022-09-21 18:01:12 +0800

    目前只能使用这种方式来进行启动了

--- bootstrap/src/main/java/org/expensive/bootstrap/TimelineApplication.java
+++ bootstrap/src/main/java/org/expensive/bootstrap/TimelineApplication.java
@@ -1,22 +1,13 @@
 package org.expensive.bootstrap;
 
-import org.expensive.common.utils.SpringContextUtil;
 import org.expensive.layout.jframe.FrameProxy;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.stereotype.Component;
 
-@Component
 public class TimelineApplication {
 
-    @Autowired
-    private static SpringContextUtil springContextUtil;
-
     public static void main(String[] args) {
-
-        springContextUtil.setApplicationContext(new ClassPathXmlApplicationContext("applicationContext.xml"));
-//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        FrameProxy frameProxy = springContextUtil.getBean("frameProxy", FrameProxy.class);
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        FrameProxy frameProxy = context.getBean("frameProxy", FrameProxy.class);
         frameProxy.initLoopShow(frameProxy);
         frameProxy.frameShow();
     }
